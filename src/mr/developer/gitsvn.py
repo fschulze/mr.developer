7d94838aa9a2e448d4c53eac2b51860310f9770d
Tom Lazar <tom@tomster.org>
2010-03-14 22:17:19 +0100

    We can use these without modification from the super class, so we don't need their definitions in this class

--- src/mr/developer/gitsvn.py
+++ src/mr/developer/gitsvn.py
@@ -49,50 +49,6 @@ class GitSVNWorkingCopy(SVNWorkingCopy):
     def svn_update(self, source, **kwargs):
         return self._svn_error_wrapper(self._svn_update, source, **kwargs)
 
-    def checkout(self, source, **kwargs):
-        name = source['name']
-        path = source['path']
-        update = self.should_update(source, **kwargs)
-        if os.path.exists(path):
-            matches = self.matches(source)
-            if matches:
-                if update:
-                    self.update(source, **kwargs)
-                else:
-                    self.output((logger.info, "Skipped checkout of existing package '%s'." % name))
-            else:
-                if self.status(source) == 'clean':
-                    return self.svn_switch(source, **kwargs)
-                else:
-                    raise GitSVNError("Can't switch package '%s' from '%s', because it's dirty." % (name, source['url']))
-        else:
-            return self.svn_checkout(source, **kwargs)
-
-    def matches(self, source):
-        info = self._svn_info(source)
-        url = source['url']
-        rev = info.get('revision')
-        match = re.search('^(.+)@(\\d+)$', url)
-        if match:
-            url = match.group(1)
-            rev = match.group(2)
-        if 'rev' in source and 'revision' in source:
-            raise ValueError("The source definition of '%s' contains duplicate revision option." % source['name'])
-        elif ('rev' in source or 'revision' in source) and match:
-            raise ValueError("The url of '%s' contains a revision and there is an additional revision option." % source['name'])
-        elif 'rev' in source:
-            rev = source['rev']
-        elif 'revision' in source:
-            rev = source['revision']
-        if url.endswith('/'):
-            url = url[:-1]
-        if rev.startswith('>='):
-            return (info.get('url') == url) and (int(info.get('revision')) >= int(rev[2:]))
-        elif rev.startswith('>'):
-            return (info.get('url') == url) and (int(info.get('revision')) > int(rev[1:]))
-        else:
-            return (info.get('url') == url) and (info.get('revision') == rev)
-
     def status(self, source, **kwargs):
         name = source['name']
         path = source['path']
@@ -125,18 +81,5 @@ class GitSVNWorkingCopy(SVNWorkingCopy):
         else:
             return status
 
-    def update(self, source, **kwargs):
-        name = source['name']
-        path = source['path']
-        force = kwargs.get('force', False)
-        status = self.status(source)
-        if not self.matches(source):
-            if force or status == 'clean':
-                return self.svn_switch(source, **kwargs)
-            else:
-                raise GitSVNError("Can't switch package '%s', because it's dirty." % name)
-        if status != 'clean' and not force:
-            raise GitSVNError("Can't update package '%s', because it's dirty." % name)
-        return self.svn_update(source, **kwargs)
 
 common.workingcopytypes['gitsvn'] = GitSVNWorkingCopy
