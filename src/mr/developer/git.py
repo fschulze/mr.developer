60c0cd54bcdaf541d50e69b4a2c39f14f7702829
Simone Deponti <shywolf9982@gmail.com>
2010-03-15 17:05:37 +0100

    Major refactoring to have different classes for different APIs

--- src/mr/developer/git.py
+++ src/mr/developer/git.py
@@ -2,6 +2,7 @@ from mr.developer import common
 import os
 import subprocess
 import re
+import sys
 
 
 logger = common.logger
@@ -12,58 +13,10 @@ class GitError(common.WCError):
 
 
 class GitWorkingCopy(common.BaseWorkingCopy):
-    def __init__(self, source):
-        super(GitWorkingCopy, self).__init__(source)
-        # determines git version as API has been jumping up and down
-        # this could also be ran at import time.
-        cmd = subprocess.Popen(["git", "--version"],
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
-        stdout, stderr = cmd.communicate()
-        if cmd.returncode != 0:
-            raise GitError("git version failed.\n%s" % (stderr,))
-        m = re.search("git version ([0-9]{1}\.[0-9]+)", stdout)
-        if m is None:
-            raise GitError("could not parse git output: %s" % (stdout,))
-        self.git_version = m.group(1)
-        
-    def git_switch_branch(self, source, stdout_in, stderr_in):
-        name = source['name']
-        path = source['path']
-        branch = source['branch']
-        if self.git_version == "1.6":
-            # git 1.6, smart enough to figure out
-            argv = ["git", "checkout", branch]
-        else:
-            # we have to see if the wanted branch is already local or
-            # not. Basically we will now analyze the output of the command that
-            # shows all branches (even remotes)
-            cmd = subprocess.Popen(["git", "branch", "-a"],
-                                   cwd=path,
-                                   stdout=subprocess.PIPE,
-                                   stderr=subprocess.PIPE)
-            stdout, stderr = cmd.communicate()
-            if cmd.returncode != 0:
-                raise GitError("git branch failed.\n%s" % (branch, stderr))
-            stdout_in += stdout
-            stderr_in += stderr
-            if re.search("^(\*| ) "+re.escape(branch)+"$", stdout, re.M):
-                # the branch is local, normal checkout will work
-                argv = ["git", "checkout", branch]
-            elif re.search("^  origin\/"+re.escape(branch)+"$", stdout, re.M):
-                # the branch is not local, normal checkout won't work here
-                argv = ["git", "checkout", "-b", branch, "origin/%s" % branch]
-        # runs the checkout with predetermined arguments
-        cmd = subprocess.Popen(argv,
-                               cwd=path,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
-        stdout, stderr = cmd.communicate()
-        if cmd.returncode != 0:
-            raise GitError("git checkout of branch '%s' failed.\n%s" % (branch, stderr))
-        return (stdout_in + stdout,
-                stderr_in + stderr)
-
+    """The git working copy base class, it holds methods that are not
+    git-version dependant
+    """
+    
     def git_checkout(self, source, **kwargs):
         name = source['name']
         path = source['path']
@@ -116,44 +69,6 @@ class GitWorkingCopy(common.BaseWorkingCopy):
         else:
             return self.git_checkout(source, **kwargs)
 
-    def matches(self, source):
-        name = source['name']
-        path = source['path']
-        if self.git_version == "1.6":
-            # This is the old matching code: it does not work on 1.5 due to the
-            # lack of the -v switch
-            cmd = subprocess.Popen(["git", "remote", "-v"],
-                                   cwd=path,
-                                   stdout=subprocess.PIPE,
-                                   stderr=subprocess.PIPE)
-            stdout, stderr = cmd.communicate()
-            if cmd.returncode != 0:
-                raise GitError("git remote for '%s' failed.\n%s" % (name, stderr))
-            return (source['url'] in stdout.split())
-        else:
-            # what we do here is first get the list of remotes, then do a
-            # remote show <remotename> on each: if one matches we return true,
-            # else we return false at the end (early bailout)
-            cmd = subprocess.Popen(["git", "remote"],
-                                   cwd=path,
-                                   stdout=subprocess.PIPE,
-                                   stderr=subprocess.PIPE)
-            stdout, stderr = cmd.communicate()
-            if cmd.returncode != 0:
-                raise GitError("git remote for '%s' failed.\n%s" % (name, stderr))
-            for remote in stdout.splitlines():
-                if remote != '':
-                    cmd = subprocess.Popen(["git", "remote", "show", remote],
-                                           cwd=path,
-                                           stdout=subprocess.PIPE,
-                                           stderr=subprocess.PIPE)
-                    stdout, stderr = cmd.communicate()
-                    if cmd.returncode != 0:
-                        raise GitError("git remote show %s for '%s' failed.\n%s" % (remote, name, stderr))
-                    if source['url'] in stdout:
-                        return True
-            return False
-
     def status(self, source, **kwargs):
         name = source['name']
         path = source['path']
@@ -181,4 +96,158 @@ class GitWorkingCopy(common.BaseWorkingCopy):
             raise GitError("Can't update package '%s', because it's dirty." % name)
         return self.git_update(source, **kwargs)
 
-common.workingcopytypes['git'] = GitWorkingCopy
+
+class Git15WorkingCopy(GitWorkingCopy):
+    """The git 1.5 specific API
+    """
+
+    def git_switch_branch(self, source, stdout_in, stderr_in):
+        name = source['name']
+        path = source['path']
+        branch = source['branch']
+        cmd = subprocess.Popen(["git", "branch", "-a"],
+                               cwd=path,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+        stdout, stderr = cmd.communicate()
+        if cmd.returncode != 0:
+            raise GitError("'git branch -a' failed.\n%s" % (branch, stderr))
+        stdout_in += stdout
+        stderr_in += stderr
+        if re.search("^(\*| ) "+re.escape(branch)+"$", stdout, re.M):
+            # the branch is local, normal checkout will work
+            argv = ["git", "checkout", branch]
+        elif re.search("^  origin\/"+re.escape(branch)+"$", stdout, re.M):
+            # the branch is not local, normal checkout won't work here
+            argv = ["git", "checkout", "-b", branch, "origin/%s" % branch]
+        # runs the checkout with predetermined arguments
+        cmd = subprocess.Popen(argv,
+                               cwd=path,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+        stdout, stderr = cmd.communicate()
+        if cmd.returncode != 0:
+            raise GitError("git checkout of branch '%s' failed.\n%s" % (branch, stderr))
+        return (stdout_in + stdout,
+                stderr_in + stderr)
+
+    def matches(self, source):
+        name = source['name']
+        path = source['path']
+        # what we do here is first get the list of remotes, then do a
+        # remote show <remotename> on each: if one matches we return true,
+        # else we return false at the end (early bailout)
+        cmd = subprocess.Popen(["git", "remote"],
+                               cwd=path,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+        stdout, stderr = cmd.communicate()
+        if cmd.returncode != 0:
+            raise GitError("git remote for '%s' failed.\n%s" % (name, stderr))
+        for remote in stdout.splitlines():
+            if remote != '':
+                cmd = subprocess.Popen(["git", "remote", "show", remote],
+                                       cwd=path,
+                                       stdout=subprocess.PIPE,
+                                       stderr=subprocess.PIPE)
+                stdout, stderr = cmd.communicate()
+                if cmd.returncode != 0:
+                    raise GitError("git remote show %s for '%s' failed.\n%s" % (remote, name, stderr))
+                if source['url'] in stdout:
+                    return True
+        return False
+
+
+class Git16WorkingCopy(GitWorkingCopy):
+    """The git 1.6 specific API
+    """
+
+    def git_switch_branch(self, source, stdout_in, stderr_in):
+        name = source['name']
+        path = source['path']
+        branch = source['branch']
+        # git 1.6, smart enough to figure out
+        cmd = subprocess.Popen(["git", "checkout", branch],
+                               cwd=path,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+        stdout, stderr = cmd.communicate()
+        if cmd.returncode != 0:
+            raise GitError("git checkout of branch '%s' failed.\n%s" % (branch, stderr))
+        return (stdout_in + stdout,
+                stderr_in + stderr)
+
+    def matches(self, source):
+        name = source['name']
+        path = source['path']
+        # This is the old matching code: it does not work on 1.5 due to the
+        # lack of the -v switch
+        cmd = subprocess.Popen(["git", "remote", "-v"],
+                               cwd=path,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+        stdout, stderr = cmd.communicate()
+        if cmd.returncode != 0:
+            raise GitError("git remote for '%s' failed.\n%s" % (name, stderr))
+        return (source['url'] in stdout.split())
+
+    
+def gitWorkingCopyFactory(source):
+    """This is the factory of git working copy classes: it will determine the
+    version of git and load up the one with the correct API. Any returned
+    instance is guaranted to pass isinstance(GitWorkingCopy)
+    """
+    # determines git version as API has been jumping up and down
+    # this could also be ran at import time.
+    try:
+        cmd = subprocess.Popen(["git", "--version"],
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    except OSError, e:
+        if getattr(e, 'errno', None) == 2:
+            logger.error("Couldn't find 'git' executable in your PATH.")
+            sys.exit(1)
+        raise
+
+    stdout, stderr = cmd.communicate()
+    if cmd.returncode != 0:
+        logger.error("Could not determine git version")
+        logger.error("'git --version' output was:\n%s\n%s" % (stdout, stderr))
+        sys.exit(1)
+
+    m = re.search("git version (\d+)\.(\d+)(\.\d+)?(\.\d+)?", stdout)
+    if m is None:
+        logger.error("Unable to parse git version output")
+        logger.error("'git --version' output was:\n%s\n%s" % (stdout, stderr))
+        sys.exit(1)
+    version = m.groups()
+
+    if len(version) == 4:
+        version = (
+            int(version[0]),
+            int(version[1]),
+            int(version[2][1:]),
+            int(version[3][1:])
+        )
+    elif len(version) == 3:
+        version = (
+            int(version[0]),
+            int(version[1]),
+            int(version[2][1:])
+        )
+    else:
+        version = (int(version[0]), int(version[1]))
+        
+    if version < (1,5):
+        logger.error(
+            "Git version %s is unsupported, please upgrade" % \
+                ".".join([str(v) for v in version])
+        )
+        sys.exit(1)
+    elif version > (1,5) and version < (1,6):
+        return Git15WorkingCopy(source)
+    else:
+        return Git16WorkingCopy(source)
+
+
+common.workingcopytypes['git'] = gitWorkingCopyFactory
