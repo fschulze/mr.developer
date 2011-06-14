66e32f2ff479ad245396238da84ff2396db943f2
Godefroid Chapelle <gotcha@bubblenet.be>
2011-06-14 17:33:14 +0200

    add branch support for mercurial

--- src/mr/developer/mercurial.py
+++ src/mr/developer/mercurial.py
@@ -4,8 +4,10 @@ import subprocess
 
 logger = common.logger
 
+
 class MercurialError(common.WCError):
     pass
+
 
 class MercurialWorkingCopy(common.BaseWorkingCopy):
     def hg_clone(self, **kwargs):
@@ -13,7 +15,8 @@ class MercurialWorkingCopy(common.BaseWorkingCopy):
         path = self.source['path']
         url = self.source['url']
         if os.path.exists(path):
-            self.output((logger.info, 'Skipped cloning of existing package %r.' % name))
+            self.output((logger.info,
+                'Skipped cloning of existing package %r.' % name))
             return
         self.output((logger.info, 'Cloned %r with mercurial.' % name))
         env = dict(os.environ)
@@ -25,6 +28,8 @@ class MercurialWorkingCopy(common.BaseWorkingCopy):
         if cmd.returncode != 0:
             raise MercurialError(
                 'hg clone for %r failed.\n%s' % (name, stderr))
+        if 'branch' in self.source:
+            stdout, stderr = self.hg_co_branch(stdout, stderr)
         if kwargs.get('verbose', False):
             return stdout
 
@@ -40,8 +45,41 @@ class MercurialWorkingCopy(common.BaseWorkingCopy):
         if cmd.returncode != 0:
             raise MercurialError(
                 'hg pull for %r failed.\n%s' % (name, stderr))
+        if 'branch' in self.source:
+            stdout, stderr = self.hg_co_branch(stdout, stderr)
         if kwargs.get('verbose', False):
             return stdout
+
+    def hg_get_branch(self):
+        name = self.source['name']
+        path = self.source['path']
+        env = dict(os.environ)
+        env.pop('PYTHONPATH', None)
+        cmd = subprocess.Popen(['hg', 'branch'], cwd=path,
+            env=env, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = cmd.communicate()
+        if cmd.returncode != 0:
+            raise MercurialError(
+                'hg branch for %r failed.\n%s' % (name, stderr))
+        return stdout[:-1]
+
+    def hg_co_branch(self, stdout_in, stderr_in):
+        name = self.source['name']
+        path = self.source['path']
+        branch = self.source['branch']
+        current_branch = self.hg_get_branch()
+        if current_branch == branch:
+            return stdout_in, stderr_in
+        env = dict(os.environ)
+        env.pop('PYTHONPATH', None)
+        cmd = subprocess.Popen(['hg', 'co', branch], cwd=path,
+            env=env, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = cmd.communicate()
+        if cmd.returncode != 0:
+            raise MercurialError(
+                'hg co %s for %r failed.\n%s' % (branch, name, stderr))
+        return (stdout_in + stdout,
+                stderr_in + stderr)
 
     def checkout(self, **kwargs):
         name = self.source['name']
@@ -51,7 +89,8 @@ class MercurialWorkingCopy(common.BaseWorkingCopy):
             if update:
                 self.update(**kwargs)
             elif self.matches():
-                self.output((logger.info, 'Skipped checkout of existing package %r.' % name))
+                self.output((logger.info,
+                    'Skipped checkout of existing package %r.' % name))
             else:
                 raise MercurialError(
                     'Source URL for existing package %r differs. '
@@ -74,7 +113,6 @@ class MercurialWorkingCopy(common.BaseWorkingCopy):
         return (self.source['url'] + '\n' == stdout)
 
     def status(self, **kwargs):
-        name = self.source['name']
         path = self.source['path']
         env = dict(os.environ)
         env.pop('PYTHONPATH', None)
@@ -90,7 +128,6 @@ class MercurialWorkingCopy(common.BaseWorkingCopy):
 
     def update(self, **kwargs):
         name = self.source['name']
-        path = self.source['path']
         if not self.matches():
             raise MercurialError(
                 "Can't update package %r because its URL doesn't match." %
