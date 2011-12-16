a966950fd279ad149bb880df4e7b9703b57da529
Szymon Pyżalski <szymon.pyzalski@stxnext.pl>
2011-12-16 11:53:47 +0100

    Added a rev option for mercurial
    The rev option forces a revision after both cloning and pulling

--- src/mr/developer/mercurial.py
+++ src/mr/developer/mercurial.py
@@ -10,8 +10,8 @@ class MercurialError(common.WCError):
 class MercurialWorkingCopy(common.BaseWorkingCopy):
 
     def __init__(self, source):
-        if 'branch' not in source:
-            source['branch'] = 'default'
+        source.setdefault('branch', 'default')
+        source.setdefault('rev')
         super(MercurialWorkingCopy, self).__init__(source)
 
     def hg_clone(self, **kwargs):
@@ -19,15 +19,26 @@ class MercurialWorkingCopy(common.BaseWorkingCopy):
         path = self.source['path']
         url = self.source['url']
         branch = self.source['branch']
+        rev = self.source['rev']
 
         if os.path.exists(path):
             self.output((logger.info, 'Skipped cloning of existing package %r.' % name))
             return
+        if branch != 'default':
+            if rev:
+                raise ValueError("'branch' and 'rev' parameters cannot be used "
+                    "simultanously")
+            else:
+                rev = branch
+        else:
+            rev = rev or 'default'
+
+
         self.output((logger.info, 'Cloned %r with mercurial.' % name))
         env = dict(os.environ)
         env.pop('PYTHONPATH', None)
         cmd = subprocess.Popen(
-            ['hg', 'clone', '--update', branch, '--quiet', '--noninteractive', url, path],
+            ['hg', 'clone', '--update', rev, '--quiet', '--noninteractive', url, path],
             env=env, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = cmd.communicate()
         if cmd.returncode != 0:
@@ -36,11 +47,25 @@ class MercurialWorkingCopy(common.BaseWorkingCopy):
         if kwargs.get('verbose', False):
             return stdout
 
+    def _update_to_rev(self, rev):
+        path = self.source['path']
+        name = self.source['name']
+        env = dict(os.environ)
+        cmd = subprocess.Popen(['hg', 'checkout', rev], cwd=path,
+                env=env, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = cmd.communicate()
+        if cmd.returncode:
+            raise MercurialError(
+                'hg update for %r failed.\n%s' %(name, stderr))
+        return stdout
+
     def hg_pull(self, **kwargs):
         # NOTE: we don't include the branch here as we just want to update
         # to the head of whatever branch the developer is working on
+        # However the 'rev' parameter works differently and forces revision
         name = self.source['name']
         path = self.source['path']
+        rev = self.source['rev']
         self.output((logger.info, 'Updated %r with mercurial.' % name))
         env = dict(os.environ)
         env.pop('PYTHONPATH', None)
@@ -50,6 +75,8 @@ class MercurialWorkingCopy(common.BaseWorkingCopy):
         if cmd.returncode != 0:
             raise MercurialError(
                 'hg pull for %r failed.\n%s' % (name, stderr))
+        if rev:
+            stdout += self._update_to_rev(rev)
         if kwargs.get('verbose', False):
             return stdout
 
