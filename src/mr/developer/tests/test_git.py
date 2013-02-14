a1855c41b8d507c4e0a5c706b4ee88c69e49c9c6
Patrick Gerken <do3cc@patrick-gerken.de>
2013-02-14 15:21:24 +0100

    Fix and test, dunno if thats enough though (yet)

---
+++ src/mr/developer/tests/test_git.py
@@ -0,0 +1,156 @@
+import argparse
+import os
+import sys
+import shutil
+
+import pytest
+from mock import patch
+
+from mr.developer.extension import Source
+from mr.developer.tests.utils import Process, JailSetup
+
+
+if sys.version_info < (3, 0):
+    b = lambda x: x
+else:
+    b = lambda x: x.encode('ascii')
+
+
+class MockConfig(object):
+
+    def __init__(self):
+        self.develop = {}
+
+    def save(self):
+        pass
+
+
+class MockDevelop(object):
+
+    def __init__(self):
+        self.always_accept_server_certificate = True
+        self.always_checkout = False
+        self.auto_checkout = ''
+        self.sources_dir = ''
+        self.develeggs = ''
+        self.config = MockConfig()
+        self.parser = argparse.ArgumentParser()
+        self.parsers = self.parser.add_subparsers(title="commands", metavar="")
+        self.threads = 1
+
+
+class GitTests(JailSetup):
+
+    def setUp(self):
+        JailSetup.setUp(self)
+
+    def testUpdateWithRevisionPin(self):
+        from mr.developer.develop import CmdCheckout
+        from mr.developer.develop import CmdUpdate
+        from mr.developer.develop import CmdStatus
+        repository = os.path.join(self.tempdir, 'repository')
+        os.mkdir(repository)
+        process = Process(cwd=repository)
+        rc, lines = process.popen(
+            "git init %s" % repository)
+        assert rc == 0
+        foo = os.path.join(repository, 'foo')
+        self.mkfile(foo, 'foo')
+        rc, lines = process.popen(
+            "git add %s" % foo,
+            echo=False)
+        assert rc == 0
+
+        rc, lines = process.popen(
+            "git commit -m 'Initial'",
+            echo=False)
+        assert rc == 0
+
+        # create branch for testing
+        rc, lines = process.popen(
+            "git checkout -b test",
+            echo=False)
+        assert rc == 0
+
+        foo2 = os.path.join(repository, 'foo2')
+        self.mkfile(foo2, 'foo2')
+        rc, lines = process.popen(
+            "git add %s" % foo2,
+            echo=False)
+        assert rc == 0
+
+        rc, lines = process.popen(
+            "git commit -m foo",
+            echo=False)
+        assert rc == 0
+
+        # get comitted rev
+        rc, lines = process.popen(
+            "git log",
+            echo=False)
+        assert rc == 0
+        rev = lines[0].split()[1]
+
+        # return to default branch
+        rc, lines = process.popen(
+            "git checkout master",
+            echo=False)
+        assert rc == 0
+
+        bar = os.path.join(repository, 'bar')
+        self.mkfile(bar, 'bar')
+        rc, lines = process.popen(
+            "git add %s" % bar,
+            echo=False)
+        assert rc == 0
+        rc, lines = process.popen(
+            "git commit -m bar",
+            echo=False)
+        assert rc == 0
+        src = os.path.join(self.tempdir, 'src')
+        os.mkdir(src)
+        develop = MockDevelop()
+        develop.sources_dir = src
+
+        # check rev
+        develop.sources = {
+            'egg': Source(
+                kind='git',
+                name='egg',
+                rev=rev,
+                url='%s' % repository,
+                path=os.path.join(src, 'egg'))}
+        CmdCheckout(develop)(develop.parser.parse_args(['co', 'egg']))
+        assert set(os.listdir(os.path.join(src, 'egg'))) == set(('.git', 'foo', 'foo2'))
+        CmdUpdate(develop)(develop.parser.parse_args(['up', 'egg']))
+        assert set(os.listdir(os.path.join(src, 'egg'))) == set(('.git', 'foo', 'foo2'))
+
+        shutil.rmtree(os.path.join(src, 'egg'))
+
+        # check branch
+        develop.sources = {
+            'egg': Source(
+                kind='git',
+                name='egg',
+                branch='test',
+                url='%s' % repository,
+                path=os.path.join(src, 'egg'))}
+        CmdCheckout(develop)(develop.parser.parse_args(['co', 'egg']))
+        assert set(os.listdir(os.path.join(src, 'egg'))) == set(('.git', 'foo', 'foo2'))
+        CmdUpdate(develop)(develop.parser.parse_args(['up', 'egg']))
+        assert set(os.listdir(os.path.join(src, 'egg'))) == set(('.git', 'foo', 'foo2'))
+
+        CmdStatus(develop)(develop.parser.parse_args(['status']))
+
+        # we can't use both rev and branch
+        pytest.raises(SystemExit, """
+            develop.sources = {
+                'egg': Source(
+                    kind='git',
+                    name='egg',
+                    branch='test',
+                    rev=rev,
+                    url='%s' % repository,
+                    path=os.path.join(src, 'egg-failed'))}
+            CmdCheckout(develop)(develop.parser.parse_args(['co', 'egg']))
+        """)
