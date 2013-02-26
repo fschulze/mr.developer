cbbc115c1fcb050503654b926b5316aa88d8aa09
Florian Schulze <florian.schulze@gmx.net>
2013-02-26 14:24:17 +0100

    Add test from submodules branch.

--- src/mr/developer/tests/test_git.py
+++ src/mr/developer/tests/test_git.py
@@ -154,3 +154,53 @@ class GitTests(JailSetup):
                     path=os.path.join(src, 'egg-failed'))}
             CmdCheckout(develop)(develop.parser.parse_args(['co', 'egg']))
         """)
+
+    def testUpdateWithoutRevisionPin(self):
+        from mr.developer.develop import CmdCheckout
+        from mr.developer.develop import CmdUpdate
+        repository = os.path.join(self.tempdir, 'repository')
+        os.mkdir(repository)
+        process = Process(cwd=repository)
+        rc, lines = process.popen(
+            "git init")
+        assert rc == 0
+        foo = os.path.join(repository, 'foo')
+        self.mkfile(foo, 'foo')
+        rc, lines = process.popen(
+            "git add %s" % foo,
+            echo=False)
+        assert rc == 0
+        rc, lines = process.popen(
+            "git commit %s -m foo" % foo,
+            echo=False)
+        assert rc == 0
+        bar = os.path.join(repository, 'bar')
+        self.mkfile(bar, 'bar')
+        rc, lines = process.popen(
+            "git add %s" % bar,
+            echo=False)
+        assert rc == 0
+        rc, lines = process.popen(
+            "git commit %s -m bar" % bar,
+            echo=False)
+        assert rc == 0
+        src = os.path.join(self.tempdir, 'src')
+        develop = MockDevelop()
+        develop.sources = {
+            'egg': Source(
+                kind='git',
+                name='egg',
+                url='file:///%s' % repository,
+                path=os.path.join(src, 'egg'))}
+        _log = patch('mr.developer.git.logger')
+        log = _log.__enter__()
+        try:
+            CmdCheckout(develop)(develop.parser.parse_args(['co', 'egg']))
+            assert set(os.listdir(os.path.join(src, 'egg'))) == set(('.git', 'bar', 'foo'))
+            CmdUpdate(develop)(develop.parser.parse_args(['up', 'egg']))
+            assert set(os.listdir(os.path.join(src, 'egg'))) == set(('.git', 'bar', 'foo'))
+            assert log.method_calls == [
+                ('info', ("Cloned 'egg' with git.",), {}),
+                ('info', ("Updated 'egg' with git.",), {})]
+        finally:
+            _log.__exit__()
