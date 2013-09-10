0f8d8cf8ac715b431a2583c525525996d9a8a249
Florian Schulze <florian.schulze@gmx.net>
2013-09-10 12:03:57 +0200

    Remove `-d` switch to use setuptools instead of distribute now that they are merged and using the switch caused bootstrap to fail.

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,15 +9,15 @@ else
 fi
 if echo $PYTHON_VERSION | grep -q " 2.4"; then
     echo $PYTHON $PYTHON_VERSION 2.4
-    $PYTHON bootstrap.py -d
+    $PYTHON bootstrap.py
     ./bin/buildout buildout:parts=test versions:zc.buildout=1.7.1
 elif echo $PYTHON_VERSION | grep -q " 2.5"; then
     echo $PYTHON $PYTHON_VERSION 2.5
-    $PYTHON bootstrap.py -d
+    $PYTHON bootstrap.py
     ./bin/buildout buildout:parts=test versions:zc.buildout=1.7.1
 elif echo $PYTHON_VERSION | grep -q " 2."; then
     echo $PYTHON $PYTHON_VERSION 2.x
-    $PYTHON bootstrap.py -d
+    $PYTHON bootstrap.py
     ./bin/buildout buildout:parts=test
 else
     echo $PYTHON $PYTHON_VERSION 3.x
