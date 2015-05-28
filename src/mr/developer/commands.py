e38e582cbefa3b3f8fe917b02948815cd071c0b3
Gil Forcada <gforcada@gnome.org>
2015-05-28 22:55:12 +0200

    Add shell command
    Runs an arbitrary command on all active sources.
    
    Fixes https://github.com/fschulze/mr.developer/issues/90

--- src/mr/developer/commands.py
+++ src/mr/developer/commands.py
@@ -619,6 +619,29 @@ class CmdReset(Command):
         config.save()
 
 
+class CmdShell(Command):
+    def __init__(self, develop):
+        Command.__init__(self, develop)
+        description = "Runs the given shell command on active sources."
+        self.parser = self.develop.parsers.add_parser(
+            "shell",
+            description=description)
+        self.develop.parsers._name_parser_map["s"] = self.develop.parsers._name_parser_map["shell"]
+        self.develop.parsers._choices_actions.append(ChoicesPseudoAction(
+            "shell", "s", help=description))
+        self.parser.add_argument(dest="cmd",
+                                 help="The command to run, i.e. 'git grep SOMETHING'.")
+        self.parser.set_defaults(func=self)
+
+    def __call__(self, args):
+        packages = self.get_packages(None, develop=True)
+        for name in sorted(packages):
+            source = self.develop.sources[name]
+            if os.path.isdir(source['path']):
+                os.chdir(source['path'])
+                os.system(args.cmd)
+
+
 class CmdStatus(Command):
     def __init__(self, develop):
         Command.__init__(self, develop)
