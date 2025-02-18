6b5f57fa2adcf39e66c315408b78f8ae2913548c
Mauro Amico <mauro.amico@gmail.com>
2025-02-18 15:57:19 +0100

    fix tests

--- src/mr/developer/tests/test_mercurial.py
+++ src/mr/developer/tests/test_mercurial.py
@@ -1,6 +1,6 @@
 import os
 
-# import pytest
+import pytest
 from mock import patch
 
 from mr.developer.extension import Source
@@ -91,30 +91,60 @@ class TestMercurial:
         CmdUpdate(develop)(develop.parser.parse_args(['up', 'egg']))
         assert set(os.listdir(os.path.join(src, 'egg'))) == set(('.hg', 'foo'))
 
-        # TODO: double check this test
+    def testUpdateWithBranch(self, develop, src, tempdir):
+        from mr.developer.commands import CmdCheckout
+        from mr.developer.commands import CmdUpdate
+        repository = tempdir['repository']
+        os.mkdir(repository)
+        process = Process(cwd=repository)
+        process.check_call("hg init %s" % repository)
+        foo = repository['foo']
+        foo.create_file('foo')
+        process.check_call("hg add %s" % foo, echo=False)
+        process.check_call("hg branch test", echo=False)
+        process.check_call("hg commit %s -m foo -u test" % foo, echo=False)
 
         # check branch
-        # develop.sources = {
-        #     'egg': Source(
-        #         kind='hg',
-        #         name='egg',
-        #         branch='test',
-        #         url='%s' % repository,
-        #         path=os.path.join(src, 'egg'))}
-        # CmdCheckout(develop)(develop.parser.parse_args(['co', 'egg']))
-        # assert set(os.listdir(os.path.join(src, 'egg'))) == set(('.hg', 'foo'))
-        # CmdUpdate(develop)(develop.parser.parse_args(['up', 'egg']))
-        # assert set(os.listdir(os.path.join(src, 'egg'))) == set(('.hg', 'foo'))
+        develop.sources = {
+            'egg': Source(
+                kind='hg',
+                name='egg',
+                branch='test',
+                url='%s' % repository,
+                path=os.path.join(src, 'egg'))}
+        CmdCheckout(develop)(develop.parser.parse_args(['co', 'egg']))
+        assert set(os.listdir(os.path.join(src, 'egg'))) == set(('.hg', 'foo'))
+        CmdUpdate(develop)(develop.parser.parse_args(['up', 'egg']))
+        assert set(os.listdir(os.path.join(src, 'egg'))) == set(('.hg', 'foo'))
+
+    def testUpdateRaiseWithRevAndBranch(self, develop, src, tempdir):
+        from mr.developer.commands import CmdCheckout
+        repository = tempdir['repository']
+        os.mkdir(repository)
+        process = Process(cwd=repository)
+        process.check_call("hg init %s" % repository)
+        foo = repository['foo']
+        foo.create_file('foo')
+        process.check_call("hg add %s" % foo, echo=False)
+        process.check_call("hg branch test", echo=False)
+        process.check_call("hg commit %s -m foo -u test" % foo, echo=False)
+
+        # get comitted rev
+        lines = process.check_call("hg log %s" % foo, echo=False)
+        try:
+            # XXX older version
+            rev = lines[0].split()[1].split(b(':'))[1]
+        except Exception:
+            rev = lines[0].split()[1]
 
         # we can't use both rev and branch
-        # TODO: doublechek: python > 3.7 raise argparse.ArgumentError insetead of SystemExit
-        # with pytest.raises(SystemExit):
-        #     develop.sources = {
-        #         'egg': Source(
-        #             kind='hg',
-        #             name='egg',
-        #             branch='test',
-        #             rev=rev,
-        #             url='%s' % repository,
-        #             path=os.path.join(src, 'egg-failed'))}
-        #     CmdCheckout(develop)(develop.parser.parse_args(['co', 'egg']))
+        with pytest.raises(SystemExit):
+            develop.sources = {
+                'egg': Source(
+                    kind='hg',
+                    name='egg',
+                    branch='test',
+                    rev=rev,
+                    url='%s' % repository,
+                    path=os.path.join(src, 'egg-failed'))}
+            CmdCheckout(develop)(develop.parser.parse_args(['co', 'egg']))
